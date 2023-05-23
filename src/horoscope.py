811f828bc75bec2a577410274117d68e8b0b8f6a
silvia <silviavergara.14sa@gmail.com>
2023-05-23 18:27:11 -0400

    original

--- src/horoscope.py
+++ src/horoscope.py
@@ -1,40 +1,42 @@
-horoscope_sign = input("Tell me your zodiacal sign: \n").lower().strip()
+horoscope_sign = input("Tell me your zodiacal sign: \n").lower().strip
 
 if(horoscope_sign =="aries"):
     print("You will find it easy to persuade people to follow your lead today but can you be sure you are doing the right thing? A testing Jupiter-Pluto link warns if you take too much for granted and make a mistake the powers that be will not be amused.")
 
 elif(horoscope_sign =="taurus"):
-    print("You may want nothing more than to help other people, but you must make sure that in helping them you don't harm yourself. Taurus is supposed to be a worldly sign, so why would you want to give away what it took so long for you to possess?")
+    print("You may want nothing more than to help other people, but you must make sure that in helping them you don’t harm yourself. Taurus is supposed to be a worldly sign, so why would you want to give away what it took so long for you to possess?")
 
 elif(horoscope_sign =="gemini"):
-    print("You cannot avoid an upheaval that is coming your way but the good news is it will result mainly in changes you find wholly beneficial. Go with the flow and let it happen and don't be surprised if the experience turns out to be a good one.")
+    print ("You cannot avoid an upheaval that is coming your way but the good news is it will result mainly in changes you find wholly beneficial. Go with the flow and let it happen and don’t be surprised if the experience turns out to be a good one.")
 
 elif(horoscope_sign =="cancer"):
-    print("Events that have been taking place behind the scenes will become public knowledge today and you will realize why loved ones have been acting so strangely of late. You may not entirely approve of their choices but you will support them anyway.")
+    print ("Events that have been taking place behind the scenes will become public knowledge today and you will realize why loved ones have been acting so strangely of late. You may not entirely approve of their choices but you will support them anyway.")
 
 elif(horoscope_sign =="leo"):
-    print("Jupiter's influence in the career area of your chart means you are desperate to prove yourself, but as power planet Pluto is strong as well you must tread carefully when dealing with employers and senior colleagues. They could see you as a threat.")
+    print("Jupiter’s influence in the career area of your chart means you are desperate to prove yourself, but as power planet Pluto is strong as well you must tread carefully when dealing with employers and senior colleagues. They could see you as a threat.")
 
 elif(horoscope_sign =="virgo"):
-    print("The message of the stars is that you need to back off a bit and let things develop at their own pace. A whole new world of possibilities is about to open up for you but if your mind is all over the place you could easily miss them!")
+    print ("The message of the stars is that you need to back off a bit and let things develop at their own pace. A whole new world of possibilities is about to open up for you but if your mind is all over the place you could easily miss them!")
 
 elif(horoscope_sign =="libra"):
-    print("The more a colleague tries to persuade you that a certain course of action is in your best interests the more you must be on your guard. The planets warn they are thinking mainly about their own professional and financial gain and very little about yours.")
+    print ("The more a colleague tries to persuade you that a certain course of action is in your best interests the more you must be on your guard. The planets warn they are thinking mainly about their own professional and financial gain and very little about yours.")
 
 elif(horoscope_sign =="scorpio"):
-    print("There are so many interesting things going on in your world at the moment but if you are smart you will put them on the back burner today and focus on a relationship issue. Deal with it right away or it could become too chaotic to control.")
+    print ("There are so many interesting things going on in your world at the moment but if you are smart you will put them on the back burner today and focus on a relationship issue. Deal with it right away or it could become too chaotic to control.")
 
 elif(horoscope_sign =="sagittarius"):
-    print("Do you feel comfortable with what you are doing? Then carry on doing it and ignore those who insist that you should be doing something else. Promise them you will take their concerns into account, then do what you were going to do anyway.")
+    print ("Do you feel comfortable with what you are doing? Then carry on doing it and ignore those who insist that you should be doing something else. Promise them you will take their concerns into account, then do what you were going to do anyway.")
 
 elif(horoscope_sign =="capricorn"):
     print("You may want to give a friend your full support but you have this nagging idea that if you do you could make their situation worse than it is now. Support them emotionally by all means but beyond that let them work it out for themselves.")
 
 elif(horoscope_sign =="aquarius"):
-    print("You are in no mood to forgive those who doubt you or, worse, have taken advantage of your generous nature. Today's Jupiter-Pluto link means there needs to be some sort of payback. At the very least let them know you are not remotely amused.")
+    print("You are in no mood to forgive those who doubt you or, worse, have taken advantage of your generous nature. Today’s Jupiter-Pluto link means there needs to be some sort of payback. At the very least let them know you are not remotely amused.")
 
 elif(horoscope_sign =="pisces"):
-    print("You have so much energy at your disposal but are you using it in positive ways? Find a quiet place where you can be on your own today and ask yourself some searching questions – and don't move ahead again until you are sure of the answers.")
-
+    print("You have so much energy at your disposal but are you using it in positive ways? Find a quiet place where you can be on your own today and ask yourself some searching questions – and don’t move ahead again until you are sure of the answers.")
+    
 else:
     print("Sign not found. ¡Tell me a valid option!")
+
+
